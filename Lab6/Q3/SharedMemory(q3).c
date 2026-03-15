69e940e7253da05c92cc1ea005d4e61d4ad2f942
MAXRIZZ_ABHIJEET <86531959+Abhijeet-Chandra@users.noreply.github.com>
2026-03-15 18:44:44 +0530

    Refactor shared memory implementation for character handling

--- Lab6/Q3/SharedMemory(q3).c
+++ Lab6/Q3/SharedMemory(q3).c
@@ -1,26 +1,39 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
-#include<sys/msg.h>
-#include<sys/wait.h>
 #include<sys/shm.h>
 #include<unistd.h>
 
+typedef struct shared{
+    int flag;
+    char a;
+}shared;
 
 int main(){
-	pid_t pid;
-	int shmid = shmget(1235, sizeof(char), IPC_CREAT | 0666);
-	char *a = (char*) shmat(shmid, NULL, 0);
+    pid_t pid;
+    int shmid = shmget(1999, sizeof(shared), IPC_CREAT | 0666);
+    shared* sh = (shared*)shmat(shmid, NULL, 0);
 
-	pid = fork();
-	if(pid == 0){
-		sleep(5);
-		printf("Successor is: %c", (char)(a[0]+1)); 
-	}
-	else{
-		printf("Enter a character: ");
-		scanf("%s", a);
-	}
-	shmdt(a);
-	shmctl(shmid, IPC_RMID, NULL);
+    sh->flag = 0;
+
+    pid = fork();
+
+    if(pid<0){
+        printf("Khatam tata bye bye good bye!");
+    }
+    else if(pid == 0){
+        while(sh->flag == 0);
+        printf("Child received %c\n",sh->a);
+        sh->a = (char)(sh->a+1);
+        sh->flag = 0;
+    }
+    else{
+        printf("Enter a character: ");
+        scanf(" %c",&(sh->a));
+        sh->flag = 1;
+        while(sh->flag == 1);
+        printf("Next character: %c\n",sh->a);
+    }
+
+    shmdt(sh);
+    shmctl(shmid, IPC_RMID, NULL);
 }
